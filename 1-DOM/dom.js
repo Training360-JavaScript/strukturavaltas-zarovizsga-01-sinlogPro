04be05430f468063d39d1339d1191426862f510a
Joe <jp.szonyegi@gmail.com>
2022-02-28 14:27:43 +0100

    1_2 solved

--- 1-DOM/dom.js
+++ 1-DOM/dom.js
@@ -1,5 +1,11 @@
 function manipulateDom() {
     // 
+    const quote = document.querySelectorAll('.quote');
+    [...quote].map(item => {
+        item.style.borderColor = 'blue';
+        item.style.backgroundColor = 'lightblue';
+        item.style.fontStyle = 'italic';        
+    })
 };
 
 export { manipulateDom };
